edd978dfb0ba3ea19fd0109d66681cbeccdcf4f3
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2023-03-15 17:44:13 +0200

    imgFix

--- src/components/test/Training.tsx
+++ src/components/test/Training.tsx
@@ -2,8 +2,40 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { Box } from '@mui/material';
+import axios from 'axios';
 
-export default function WovenImageList() {
+interface PixabayImage {
+  id: number;
+  webformatURL: string;
+  largeImageURL: string;
+  tags: string;
+  user: string;
+  views: number;
+  comments: number;
+  downloads: number;
+}
+
+interface PixabayAPIResponse {
+  hits: PixabayImage[];
+  total: number;
+  totalHits: number;
+}
+
+const WovenImageList = () => {
+  const [images, setImages] = React.useState<PixabayImage[]>([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getArticles(1, 'windsurfing'); // Параметры для запроса
+        setImages(response.hits);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Box
       sx={{
@@ -23,12 +55,12 @@ export default function WovenImageList() {
         cols={3}
         gap={8}
       >
-        {itemData.map(item => (
-          <ImageListItem key={item.img}>
+        {images.map(item => (
+          <ImageListItem key={item.id}>
             <img
-              src={`${item.img}?w=161&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=161&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
+              src={`${item.webformatURL}?w=161&fit=crop&auto=format`}
+              srcSet={`${item.webformatURL}?w=161&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.tags}
               loading="lazy"
             />
           </ImageListItem>
@@ -36,55 +68,31 @@ export default function WovenImageList() {
       </ImageList>
     </Box>
   );
-}
+};
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
-    title: 'Bed',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
-    title: 'Kitchen',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
-    title: 'Sink',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
-    title: 'Books',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
-    title: 'Chairs',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
-    title: 'Candle',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
-    title: 'Laptop',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
-    title: 'Doors',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
-    title: 'Coffee',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
-    title: 'Storage',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
-    title: 'Coffee table',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
-    title: 'Blinds',
-  },
-];
+export default WovenImageList;
+
+export const getArticles = async (
+  page: number,
+  query: string,
+): Promise<PixabayAPIResponse> => {
+  const URL = 'https://pixabay.com/api/';
+  const options = new URLSearchParams({
+    key: '28584763-421dc035e00a550bd9f3576d4',
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    page: page.toString(),
+    per_page: '21',
+  });
+
+  try {
+    const response = await axios.get<PixabayAPIResponse>(`${URL}?${options}`);
+    if (response.data.hits.length === 0) {
+      throw new Error('No results found');
+    }
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
